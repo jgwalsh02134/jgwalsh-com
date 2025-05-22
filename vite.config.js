6574a1a5e792105757054d7fa86917a10f5dede0
JG Walsh <jgregorywalsh@icloud.com>
2025-05-22 05:47:55 -0400

    Update base path in Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jgwalsh-com/',
+  base: '/',
   server: {
     host: true,
     port: 4173
