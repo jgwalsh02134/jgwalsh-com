9b36c02f096915da0c543a2a170bc0594f158401
JG Walsh <jgregorywalsh@icloud.com>
2025-05-22 06:10:28 -0400

    Fix base path for Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  server: {
-    host: true,
-    port: 4173
-  },
-  plugins: [react()]
+  base: '/', // required for correct asset paths on Cloudflare Pages
+  plugins: [react()],
 })
