3df03dc0a9072a4a825403c41da3da74c035f953
JG Walsh <jgregorywalsh@icloud.com>
2025-05-22 05:44:43 -0400

    Refactor Vite configuration to improve server settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jgwalsh-com/', // <-- set this if deploying to GitHub Pages
-  plugins: [react()],
+  base: '/jgwalsh-com/',
+  server: {
+    host: true,
+    port: 4173
+  },
+  plugins: [react()]
 })
